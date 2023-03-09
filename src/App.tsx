67733c72c72610352758c8fcaf7d6892e4d4e582
Nate Card <ncard24@gmail.com>
2023-03-08 22:54:30 -0500

    Projects section filled in needs to be styled.

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ function App() {
 				<AboutMe />
 			</div>
 			<div className=" snap-center snap-always pb-96">
+				<h1 className="text-center text-6xl">My Projects</h1>
 				<Projects
 					title={'Subforuma'}
 					img={subforuma}
@@ -28,7 +29,17 @@ function App() {
 					language2={'React'}
 					language3Icon={SiTailwindcss}
 					language3={'TailwindCSS'}
-					readme={''}
+					readme={`Features
+					Toggle dark / light theme based on system theme
+					Create, reply, like public posts
+					PostgreSQL database and realtime functions
+					Responsive design
+					Google Sign In
+					Sign Out
+					Magic Link Sign In
+					For Google Sign In, I used Supabase Auth. This allowed for a good base to pull user data and display profile pictures, display names, etc to build out the basic "tweets"
+					
+					The PostgreSQL database from Supabase gave me the ability to build my own SQL functions to render the replies in the timeline. By doing this I was able to sort and sequence the replies by the number of likes that each had received, simulating a sort of social media algorithm promoting more liked content to the top of the replies.`}
 					githubUrl={'https://github.com/natecard/microblog'}
 					liveDemo={''}
 				/>
@@ -43,7 +54,15 @@ function App() {
 					language2={'React'}
 					language3Icon={SiTailwindcss}
 					language3={'TailwindCSS'}
-					readme={''}
+					readme={`I built an ecommerce site to utilize the recently released Shopify Mock Store API
+
+					This was built using Vite, React, TypeScript, and CSS done using TailwindCSS.
+					
+					Live Site Demo
+					
+					It is a simple ecommerce store with landing page, store page, individual item pages/modals, and shopping cart.
+					
+					`}
 					githubUrl={'https://github.com/natecard/FakeStore'}
 					liveDemo={''}
 				/>
@@ -58,7 +77,7 @@ function App() {
 					language2={'React'}
 					language3Icon={SiTailwindcss}
 					language3={'TailwindCSS'}
-					readme={''}
+					readme={``}
 					githubUrl={'https://github.com/natecard/portfolio-site'}
 					liveDemo={''}
 				/>
--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -4,32 +4,38 @@ import { Link } from 'react-router-dom';
 
 export default function Projects(props: projects) {
 	return (
-		<div className="flex flex-col">
-			<div className="">
+		<div className="flex flex-col ">
+			<div className="flex flex-col items-center">
 				<h2 className="pb-8 text-5xl">{props.title}</h2>
-				<img className="flex h-3/6 w-5/6" src={props.img} alt="project screen shot" />
-				<div className=" flex flex-row justify-center">
-					<div className="m-4 h-8 w-24 rounded-md">
-						<Link to={`https://www.typescriptlang.org/`}>
-							<SiTypescript />
-						</Link>
-						<p>{props.language1}</p>
-					</div>
-					<div className=" m-4 h-8 w-24 rounded-md">
-						<Link to={`https://reactjs.org/`}>
-							<SiReact />
-						</Link>
-						<p>{props.language2}</p>
-					</div>
-					<div className="m-4 h-8 w-24 rounded-md">
-						<Link to={`https://tailwindcss.com/`}>
-							<SiTailwindcss />
-						</Link>
-						<p>{props.language3}</p>
-					</div>
+				<img
+					className="flex h-3/6 w-5/6 items-center rounded-lg"
+					src={props.img}
+					alt="project screen shot"
+				/>
+			</div>
+			<div className=" flex flex-row justify-evenly pt-4">
+				<div className="flex flex-col items-center">
+					<Link to={`https://www.typescriptlang.org/`}>
+						<SiTypescript />
+					</Link>
+					<p>{props.language1}</p>
 				</div>
-				<p className="w-80">{props.readme}</p>
-				<button className="h-8 w-2/6 rounded-md border-2 p-3">
+				<div className="flex flex-col items-center">
+					<Link to={`https://reactjs.org/`}>
+						<SiReact />
+					</Link>
+					<p>{props.language2}</p>
+				</div>
+				<div className="flex flex-col items-center">
+					<Link to={`https://tailwindcss.com/`}>
+						<SiTailwindcss />
+					</Link>
+					<p>{props.language3}</p>
+				</div>
+			</div>
+			<div className="flex flex-col items-center pt-4">
+				<p className="w-3/5 pb-8 text-lg">{props.readme}</p>
+				<button className="flex h-8 w-2/6 items-center justify-center rounded-md border-2 p-3 hover:bg-white hover:text-black md:w-48">
 					<Link to={props.githubUrl}> Github Repo</Link>
 				</button>
 			</div>
