8e00cd63f2c19dbc5c489950a183e160c0c98330
Nate Card <ncard24@gmail.com>
2024-03-08 15:19:30 -0500

    Update default branch name in config.ts

--- tina/config.ts
+++ tina/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tinacms';
 
 // Your hosting provider likely exposes this as an environment variable
 const branch =
-	process.env.GITHUB_BRANCH || process.env.VERCEL_GIT_COMMIT_REF || process.env.HEAD || 'main';
+	process.env.GITHUB_BRANCH || process.env.VERCEL_GIT_COMMIT_REF || process.env.HEAD || 'v2';
 
 export default defineConfig({
 	branch,
