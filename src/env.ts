92cd90e2578a021a6f9e4e2c64e487302e8c5490
Nate Card <ncard24@gmail.com>
2024-03-08 11:05:44 -0500

    Remove Contact component and add env.ts file

---
+++ src/env.ts
@@ -0,0 +1,25 @@
+// Define the required environment variables
+const requiredEnvVars = ['RECAPTCHA_SECRET_KEY'];
+
+// Check if the required environment variables are present
+const envVarsPresent = requiredEnvVars.every((envVar) => {
+	if (!process.env[envVar]) {
+		console.warn(`Environment variable ${envVar} is not defined.`);
+		return false;
+	}
+	return true;
+});
+
+// If any of the required environment variables are missing, throw an error
+if (!envVarsPresent) {
+	throw new Error(
+		`Some required environment variables are not defined. Please check your .env file.`,
+	);
+}
+
+// Export the environment variables
+export const getEnvVar = (key: string): string => {
+	return process.env[key] || '';
+};
+
+export const RECAPTCHA_SECRET_KEY = getEnvVar('RECAPTCHA_SECRET_KEY');
