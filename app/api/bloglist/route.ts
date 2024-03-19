f23699430c140dceb7af5c1fb1b52d62844dee93
Nate Card <ncard24@gmail.com>
2024-03-18 21:24:38 -0400

    Add route for fetching blog posts

--- app/api/bloglist/route.ts
+++ app/api/bloglist/route.ts
@@ -1,7 +1,8 @@
 import { client } from '@/tina/__generated__/client';
+import { NextResponse } from 'next/server';
 export async function GET() {
 	const postsResponse = await client.queries.postConnection();
 	const data = postsResponse;
 
-	return Response.json(data);
+	return NextResponse.json(data);
 }
