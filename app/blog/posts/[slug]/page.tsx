4b88e18f226330ab41b6d5e4784fad3286524470
Nate Card <ncard24@gmail.com>
2024-03-27 21:43:31 -0400

    Refactor DisplayPost function to use TinaCMS hooks

--- app/blog/posts/[slug]/page.tsx
+++ app/blog/posts/[slug]/page.tsx
@@ -1,30 +1,40 @@
+'use client';
 import client from '@/tina/__generated__/client';
 import BlogPost from '@/components/BlogPost';
 import { BlogLayoutProps } from '@/Interfaces';
+import { useTina } from 'tinacms/dist/react';
 
-export default async function DisplayPost({ params }: { params: { slug: string } }) {
-	const { slug } = params;
-	const postsResponse = await client.queries.post({ relativePath: `${slug}.md` });
-	const { data } = postsResponse;
-	const post = data.post;
+export default async function DisplayPost(
+	// { params }: { params: { slug: string } }
+	props: any,
+) {
+	// const { slug } = params;
+	// const postsResponse = await client.queries.post({ relativePath: `${slug}.md` });
+	// const { data } = postsResponse;
+	// const post = data.post;
+	const { data } = useTina({
+		query: props.query,
+		variables: props.variables,
+		data: props.data,
+	});
 	let tags: string[] = [];
-	if (post.tags) {
-		tags = post.tags.split(',');
+	if (data) {
+		tags = data.post.tags.split(',');
 	}
 	return (
 		<main>
 			<article>
 				<BlogPost
-					key={post.id}
-					title={post.title}
-					body={post.body}
-					author={post.author}
-					date={post.date}
-					excerpt={post.excerpt}
-					coverImage={post.coverImage}
-					slug={post.slug!}
+					key={data.post.id}
+					title={data.post.title}
+					body={data.post.body}
+					author={data.post.author}
+					date={data.post.date}
+					excerpt={data.post.excerpt}
+					coverImage={data.post.coverImage}
+					slug={data.post.slug!}
 					tags={tags.map((tag) => tag || '') || []}
-					id={post.id}
+					id={data.post.id}
 				/>
 			</article>
 		</main>
