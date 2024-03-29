0dbecd6d2d87106a221c7c1c87f771d070e960fb
Nate Card <ncard24@gmail.com>
2024-03-28 22:30:34 -0400

    Refactor DisplayPost function and update TinaCMS query

--- app/blog/posts/[slug]/page.tsx
+++ app/blog/posts/[slug]/page.tsx
@@ -1,4 +1,3 @@
-'use client';
 import client from '@/tina/__generated__/client';
 import BlogPost from '@/components/BlogPost';
 import { BlogLayoutProps } from '@/Interfaces';
@@ -6,35 +5,69 @@ import { useTina } from 'tinacms/dist/react';
 
 export default async function DisplayPost(
 	// { params }: { params: { slug: string } }
-	props: any,
+	{ slug }: { slug: string },
 ) {
 	// const { slug } = params;
 	// const postsResponse = await client.queries.post({ relativePath: `${slug}.md` });
 	// const { data } = postsResponse;
 	// const post = data.post;
+	// let tags: string[] = [];
+	// if (post.tags) {
+	// 	tags = post.tags.split(',');
+	// }
 	const { data } = useTina({
-		query: props.query,
-		variables: props.variables,
-		data: props.data,
+		query: `
+			query post($relativePath: String!) {
+				post(relativePath: $relativePath) {
+					... on Document {
+						_sys {
+							filename
+							basename
+							breadcrumbs
+							path
+							relativePath
+							extension
+						}
+						id
+					}
+					...PostParts
+				}
+			}
+			
+			fragment PostParts on Post {
+				__typename
+				title
+				excerpt
+				coverImage
+				date
+				author
+				slug
+				body
+			}
+		`,
+		variables: { relativePath: `${slug}.md` },
 	});
+
+	const post = data.post;
 	let tags: string[] = [];
-	if (data) {
-		tags = data.post.tags.split(',');
+	if (post.tags) {
+		tags = post.tags.split(',');
 	}
+
 	return (
 		<main>
 			<article>
 				<BlogPost
-					key={data.post.id}
-					title={data.post.title}
-					body={data.post.body}
-					author={data.post.author}
-					date={data.post.date}
-					excerpt={data.post.excerpt}
-					coverImage={data.post.coverImage}
-					slug={data.post.slug!}
+					key={post.id}
+					title={post.title}
+					body={post.body}
+					author={post.author}
+					date={post.date}
+					excerpt={post.excerpt}
+					coverImage={post.coverImage}
+					slug={post.slug!}
 					tags={tags.map((tag) => tag || '') || []}
-					id={data.post.id}
+					id={post.id}
 				/>
 			</article>
 		</main>
